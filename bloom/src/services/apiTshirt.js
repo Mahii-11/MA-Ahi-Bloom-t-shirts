2008361e8a0fc1ca3708953bd1ca520d7b4b667c
MAAhi-Bloom <czone3144@gmail.com>
2024-11-10 12:53:51 +0600

    add a new folder

---
+++ bloom/src/services/apiTshirt.js
@@ -0,0 +1,35 @@
+const API_URL = "https://localrepo-ddnm.onrender.com/api";
+
+export async function getTshirt() {
+  const res = await fetch(`${API_URL}/menu`);
+  if (!res.ok) throw Error("Failed getting t-shirts");
+  const { data } = await res.json();
+  console.log(data);
+  return data;
+}
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
+
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error("Failed creating your order");
+  }
+}
