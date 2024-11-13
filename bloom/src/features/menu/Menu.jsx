151452bb141520ae6470552a2f676b3a98f62dda
MAAhi-Bloom <czone3144@gmail.com>
2024-11-13 16:14:24 +0600

    add some changes

--- bloom/src/features/menu/Menu.jsx
+++ bloom/src/features/menu/Menu.jsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  //console.log(menu);
+  console.log(menu);
 
   return (
     <div className="container mx-auto p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 my-28">
