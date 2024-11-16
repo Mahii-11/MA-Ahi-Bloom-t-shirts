e8111200a808160448d284515125ef35cc32d6df
MAAhi-Bloom <czone3144@gmail.com>
2024-11-16 16:57:13 +0600

    Add a new changes

--- bloom/src/services/apiTshirt.js
+++ bloom/src/services/apiTshirt.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5005/api";
+const API_URL = "https://localrepo-ddnm.onrender.com/api";
 
 export async function getTshirt() {
   const res = await fetch(`${API_URL}/menu`);
